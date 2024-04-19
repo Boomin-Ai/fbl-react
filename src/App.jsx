83e95da38df9862e424a1d1badb60cf6d0c97574
Kurtney Joseph <jo@Kurtneys-MacBook-Pro.local>
2024-04-19 15:32:18 -0400

    feat: fb login

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+import {
+  initFacebookSdk,
+  fbLogin,
+  getFacebookLoginStatus,
+} from "../utils/FacebookSDK";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    console.log("Started use effect");
+    initFacebookSdk().then(() => {
+      getFacebookLoginStatus().then((response) => {
+        if (response == null) {
+          console.log("No login status for the person");
+        } else {
+          console.log(response);
+        }
+      });
+    });
+  }, []);
+
+  function login() {
+    console.log("reached log in button");
+    fbLogin().then((response) => {
+      console.log(response);
+      if (response.status === "connected") {
+        console.log("Person is connected");
+      } else {
+        console.log("Person is not connected");
+      }
+    });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={login}>Login to FB</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ utils/FacebookSDK.js
@@ -0,0 +1,29 @@
+export const initFacebookSdk = () => {
+  return new Promise((resolve, reject) => {
+    window.fbAsyncInit = () => {
+      window.FB.init({
+        appId: "829530768872787",
+        cookie: true,
+        xfbml: true,
+        version: "v19.0",
+      });
+      resolve();
+    };
+  });
+};
+
+export const getFacebookLoginStatus = () => {
+  return new Promise((resolve, reject) => {
+    window.FB.getLoginStatus((response) => {
+      resolve(response);
+    });
+  });
+};
+
+export const fbLogin = () => {
+  return new Promise((resolve, reject) => {
+    window.FB.login((response) => {
+      resolve(response);
+    });
+  });
+};
