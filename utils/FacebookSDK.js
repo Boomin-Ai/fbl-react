3e725af525c87146e99c0966300271ea05d6ee0e
Jo <josephkurt18@gmail.com>
2024-04-19 17:09:52 -0400

    refactor: fb login

--- utils/FacebookSDK.js
+++ utils/FacebookSDK.js
@@ -3,7 +3,7 @@ export const initFacebookSdk = () => {
   return new Promise((resolve, reject) => {
     window.fbAsyncInit = () => {
       window.FB.init({
-        appId: "3718972341677268",
+        appId: "791770779680859",
         cookie: true,
         xfbml: true,
         version: "v19.0",
